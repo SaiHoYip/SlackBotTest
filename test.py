dde238e701f8980c41deee9d1ce9d72857f1ae4b
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2021-11-11 19:55:21 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -2,9 +2,35 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from flask import Flask, request, Response
+from slackeventsapi import SlackEventAdapter
+
 env_path = Path('.')/ '.env'
 load_dotenv(dotenv_path= env_path)
-
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+BOT_ID = client.api_call("auth.test")['user_id']
 
-client.chat_postMessage(channel= '#general', text="Hello")
+#Simple Hello Message
+def Hello(channel_id, text):
+    if text == "fu":
+        client.chat_postMessage(channel= channel_id, text="no cursing")
+    else:
+        client.chat_postMessage(channel= channel_id, text=text)
+
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events', app)
+@slack_event_adapter.on('message')
+def message(payload):
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
+    #limits the call to users
+    if BOT_ID != user_id:
+        Hello(channel_id, text)
+
+@app.route('/message-count', methods= ['POST'])
+def message_count():
+    return Response, 200
+if __name__=="__main__":
+    app.run(debug=True)
